96dd754bdee9571e26a1511ae26385614858dc16
bruce <lixiaolongmessi@163.com>
2018-07-13 00:15:34 +0800

    recommit

---
+++ c++/bubble_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int array[], int len)
+{
+    for(int i=0; i<len-1; i++)
+    {
+        for(int j=0; j<len-i-1; j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main(int argc, char** agrv)
+{
+    int array[] = {100,88,120,66,180,68,168,888,666,999};
+    bubbleSort(array,sizeof(array)/sizeof(int));
+	for(int i=0; i<10; i++)
+		cout<<array[i]<<" ";
+	cout << endl;
+	system("pause");
+}
